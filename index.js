2533d82b24a8c5637b4e3eab1c9b5ab867934429
Cedric David <cedric.david@btopenworld.com>
2018-08-27 14:01:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger');
 
 function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
-
-
-  if (parseInt(dodger.style.left.replace('px', ''), 10) > 0) {
-    dodger.style.left = `${(parseInt(dodger.style.left.replace('px', ''), 10) - 1)}px`
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
   }
 }
 
 function moveDodgerRight() {
-  if (parseInt(dodger.style.right.replace('px', ''), 10) > 0) {
-    dodger.style.right = `${(parseInt(dodger.style.right.replace('px', ''), 10) - 1)}px`
+  var rightNumbers = dogder.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10);
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
   }
 }
 document.addEventListener('keydown', function(e) {
   if (e.which === 39) {
     moveDodgerRight()
-  }else if (e.which === 37 {
+  }else if (e.which === 37) {
     moveDodgerLeft()
   }
 })
