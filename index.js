34510303cf92f80a17e1e0a4f97b95c23d0d5b23
Cedric David <cedric.david@btopenworld.com>
2018-08-27 14:03:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function moveDodgerRight() {
   var rightNumbers = dogder.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10);
 
-  if (right > 0) {
+  if (right < 0) {
     dodger.style.right = `${right - 1}px`
   }
 }
