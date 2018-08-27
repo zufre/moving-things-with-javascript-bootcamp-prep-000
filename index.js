8422e3e43fae66305fa85127d35cc4adf1a049b6
Cedric David <cedric.david@btopenworld.com>
2018-08-27 13:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger');
 function moveDodgerRight() {
-  var rightNumbers = doger.style.right.replace('px', '')
+  var rightNumbers = dogder.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10);
 
-  if (right<0) {
+  if (right>0) {
     dodger.style.right = `${right - 1}px`
   }
 }
