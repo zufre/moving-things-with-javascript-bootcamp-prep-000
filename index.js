fded42aa54f7ee61f9656171b81cd368da5cec12
Cedric David <cedric.david@btopenworld.com>
2018-08-27 13:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,9 @@ function moveDodgerRight() {
   }
 }
 document.addEventListener('keydown', function(e) {
-  if (e.which === 38) {
+  if (e.which === 39) {
     moveDodgerRight()
   }else if (e.which === 37) {
     moveDodgerLeft()
   }
 })
-
