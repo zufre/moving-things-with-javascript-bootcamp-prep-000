560584d2fe55530f6ecc0136233a8b97f1d40bf1
Cedric David <cedric.david@btopenworld.com>
2018-08-27 14:07:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
 
-  if (dogder.style.left > 0) {
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
 }
@@ -15,7 +15,7 @@ function moveDodgerRight() {
   var rightNumbers = dogder.style.right.replace('px', '')
   var right = parseInt(rightNumbers, 10);
 
-  if (dogder.style.right > 0) {
+  if (right > 0) {
     dodger.style.right = `${right - 1}px`
   }
 }
